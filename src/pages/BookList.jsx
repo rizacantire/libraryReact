03d5c48a2138c3baa0769901f75fb46ee31fe7a0
rza35 <riza_tire@hotmail.com>
2021-08-09 01:27:23 +0300

    Library React

---
+++ src/pages/BookList.jsx
@@ -0,0 +1,54 @@
+import React, { useState,useEffect } from "react";
+import { Icon, Label, Menu, Table } from "semantic-ui-react";
+export default function BookList() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(()=>{
+    
+  })
+
+  return (
+    <div>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Kitap Adı</Table.HeaderCell>
+            <Table.HeaderCell>Yazar</Table.HeaderCell>
+            <Table.HeaderCell>Sayfa</Table.HeaderCell>
+            <Table.HeaderCell>Kategori</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {books.map((book) => (
+            <Table.Row>
+              <Table.Cell>b</Table.Cell>
+              <Table.Cell>Cell</Table.Cell>
+              <Table.Cell>Cell</Table.Cell>
+              <Table.Cell>Cell</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+
+        <Table.Footer>
+          <Table.Row>
+            <Table.HeaderCell colSpan="4">
+              <Menu floated="right" pagination>
+                <Menu.Item as="a" icon>
+                  <Icon name="chevron left" />
+                </Menu.Item>
+                <Menu.Item as="a">1</Menu.Item>
+                <Menu.Item as="a">2</Menu.Item>
+                <Menu.Item as="a">3</Menu.Item>
+                <Menu.Item as="a">4</Menu.Item>
+                <Menu.Item as="a" icon>
+                  <Icon name="chevron right" />
+                </Menu.Item>
+              </Menu>
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>
+    </div>
+  );
+}
